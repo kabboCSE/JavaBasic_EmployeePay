50fc5681511509a95693a71cd3eef7793db34a67
kabboCSE <shahriar15-10494@diu.edu.bd>
2024-09-29 17:26:27 +0600

    DoWhile

---
+++ src/main/java/loop/PayEmployeeWithBonusDoWhileLoop.java
@@ -0,0 +1,41 @@
+package loop;
+
+import java.util.Scanner;
+
+public class PayEmployeeWithBonusDoWhileLoop {
+    public static void main(String[] args) {
+        int weeklyHourRange = 30;
+        double ratePerHour = 10.0;
+        double bonusAmount = 2;
+        Scanner scanner = new Scanner(System.in);
+        String controller;
+
+
+
+        do{
+            System.out.println("Type WeeklyHour new: ");
+
+            double weeklyHour = scanner.nextDouble();
+
+            double payable = weeklyHour * ratePerHour;
+
+            if (weeklyHour >= weeklyHourRange) {
+                payable = payable + bonusAmount;
+                System.out.println("Congratulations! You are eligible for Bonus");
+            }
+            else{
+                System.out.println("You are not eligable for Bonus");
+            }
+            System.out.println("Total Payable: " + payable);
+            System.out.println("Do you want to continue Yes/No: .................");
+            controller = scanner.next();
+        } while (controller.equalsIgnoreCase("yes"));
+
+
+
+
+    }
+}
+
+
+
